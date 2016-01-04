fb905f9b5672c4744fbb9fedbaf294627c7dd6a9
sethwalsh <seth.walsh@utah.edu>
2016-01-04 09:48:50 -0700

    addming setup

---
+++ CustomInstaller/CustomInstaller.h
@@ -0,0 +1,13 @@
+// CustomInstaller.h
+
+#pragma once
+
+using namespace System;
+
+namespace CustomInstaller {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
